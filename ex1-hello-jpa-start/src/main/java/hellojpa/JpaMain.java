4e25a163a4c8dcdda5abb8d17ca124f37c23180d
tkdduf212@gmail.com <tkdduf212@gmail.com>
2024-02-22 04:27:28 +0900

    영속성 전이 테스트

--- ex1-hello-jpa-start/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa-start/src/main/java/hellojpa/JpaMain.java
@@ -15,17 +15,29 @@ public class JpaMain {
         tx.begin();
         try {
             // 영속
-            Member member = em.find(Member.class, 150L);
-            member.setName("AAAAA");
+//            Member member = em.find(Member.class, 150L);
+            Member member = new Member(1L, "AAAA");
+//            member.setName("AAAAA");
 
 //            em.detach(member);
+            em.persist(member);
+            em.flush();
             em.clear();
-            Member member2 = em.find(Member.class, 150L);
+            Member findMember = em.getReference(Member.class, member.getId());
+            System.out.println("findMember type = " + findMember.getClass());
             System.out.println("=============================================");
+            System.out.println("findMember = " + findMember.getName());
+            System.out.println("=============================================");
+            System.out.println("findMember type = " + findMember.getClass());
 
+//            Member member2 = em.find(Member.class, 150L);
+            System.out.println("=============================================");
+            Member member1 = em.find(Member.class, member.getId());
+            System.out.println("member1.getClass() = " + member1.getClass());
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
+            e.printStackTrace();
         } finally {
             em.close();
             emf.close();
