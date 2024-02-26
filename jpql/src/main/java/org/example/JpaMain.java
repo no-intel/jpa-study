e51b9169c0f366adc9cc75964d0a87c6bea45d77
tkdduf212@gmail.com <tkdduf212@gmail.com>
2024-02-27 04:35:13 +0900

    jqpl Enum 사용

--- jpql/src/main/java/org/example/JpaMain.java
+++ jpql/src/main/java/org/example/JpaMain.java
@@ -1,6 +1,9 @@
 package org.example;
 
-import jakarta.persistence.*;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
 
 import java.util.List;
 
@@ -18,17 +21,24 @@ public class JpaMain {
                 member.setUsername("member" + i);
                 member.setAge(i + 1);
                 em.persist(member);
+                if (i == 0) {
+                    member.setType(MemberType.ADMIN);
+                }
             }
 
             em.flush();
             em.clear();
 
-            List<Member> pagingResult = em.createQuery("select m from Member m order by m.age desc ", Member.class)
-                    .setFirstResult(0)
-                    .setMaxResults(10)
+            List<Object[]> resultList = em.createQuery("select m.username, 'HELLO', 'She''s', true from Member m " +
+                            "where m.type = org.example.MemberType.ADMIN", Object[].class)
                     .getResultList();
-            System.out.println("pagingResult.size() = " + pagingResult.size());
-            pagingResult.forEach(m -> System.out.println(m.toString()));
+            resultList.forEach(m -> {
+                System.out.println("m[0] = " + m[0]);
+                System.out.println("m[1] = " + m[1]);
+                System.out.println("m[2] = " + m[2]);
+                System.out.println("m[3] = " + m[3]);
+            });
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
---
+++ jpql/src/main/java/org/example/MemberType.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public enum MemberType {
+    USER, ADMIN
+}
