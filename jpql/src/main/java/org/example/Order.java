691bb75776d2676e649dc26ce7b4bf18f4a2454d
tkdduf212@gmail.com <tkdduf212@gmail.com>
2024-02-26 18:07:57 +0900

    jpal 예제 엔티티 세팅

---
+++ jpql/src/main/java/org/example/Order.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "OREDERS")
+public class Order {
+
+    @Id @GeneratedValue
+    private Long id;
+    private int orderAmount;
+
+    @Embedded
+    private Address address;
+
+    @ManyToOne
+    @JoinColumn(name = "PRODUCT_ID")
+    private Product product;
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getOrderAmount() {
+        return orderAmount;
+    }
+
+    public void setOrderAmount(Integer orderAmount) {
+        this.orderAmount = orderAmount;
+    }
+
+    public void setOrderAmount(int orderAmount) {
+        this.orderAmount = orderAmount;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
