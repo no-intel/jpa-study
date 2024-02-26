53619902550c940d14b04b77cfd5a5770eacbb68
tkdduf212@gmail.com <tkdduf212@gmail.com>
2024-02-27 01:32:12 +0900

    jpql 기본 쿼리 및 파라메터 사용

--- jpql/src/main/java/org/example/JpaMain.java
+++ jpql/src/main/java/org/example/JpaMain.java
@@ -1,9 +1,8 @@
 package org.example;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
-import jakarta.persistence.Persistence;
+import jakarta.persistence.*;
+
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -17,6 +16,15 @@ public class JpaMain {
             Member member = new Member();
             member.setUsername("member1");
             em.persist(member);
+
+            TypedQuery<Member> query = em.createQuery("select m from Member as m", Member.class);
+            List<Member> resultList = query.getResultList();
+            resultList.forEach(System.out::println);
+
+            Member parameter = em.createQuery("select m from Member AS m WHERE m.username=:parameter", Member.class)
+                    .setParameter("parameter", member.getUsername())
+                    .getSingleResult();
+            System.out.println(parameter);
 
             tx.commit();
         } catch (Exception e) {
