615998b012cf5af74fc35c4b3b7b001489831229
tkdduf212@gmail.com <tkdduf212@gmail.com>
2024-02-29 07:41:56 +0900

    fetch join

--- jpql/src/main/java/org/example/JpaMain.java
+++ jpql/src/main/java/org/example/JpaMain.java
@@ -16,22 +16,57 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            for (int i = 0; i < 100; i++) {
-                Member member = new Member();
-                member.setUsername("member" + i);
-                member.setAge(i + 1);
-                em.persist(member);
-                if (i == 0) {
-                    member.setType(MemberType.ADMIN);
-                }
-            }
+            Team teamA = new Team();
+            teamA.setName("TeamA");
+
+            Team teamB = new Team();
+            teamB.setName("TeamB");
+            em.persist(teamA);
+            em.persist(teamB);
+
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+
+            em.persist(member1);
+            em.persist(member2);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            String singleResult = em.createQuery("select group_concat(m.username) from Member m", String.class)
-                    .getSingleResult();
-            System.out.println("singleResult = " + singleResult);
+            List<Member> resultList = em.createQuery("SELECT m FROM Member m JOIN FETCH m.team", Member.class)
+                    .getResultList();
+
+            resultList.forEach(o -> System.out.println("o = " + o ));
+
+            em.clear();
+
+            List<Team> resultList1 = em.createQuery("SELECT t FROM Team t JOIN FETCH t.members", Team.class)
+                    .getResultList();
+
+            resultList1.forEach(o -> {
+                System.out.println("o = " + o.getName());
+                o.getMembers().forEach(m -> System.out.println("m.getUsername() = " + m.getUsername()));
+            });
+
+            em.clear();
+
+            List<Team> resultList2 = em.createQuery("SELECT t FROM Team t", Team.class)
+                    .setMaxResults(0)
+                    .setMaxResults(2)
+                    .getResultList();
+
+            resultList2.forEach(o -> {
+                System.out.println("o = " + o.getName());
+                o.getMembers().forEach(m -> System.out.println("m.getUsername() = " + m.getUsername()));
+            });
 
             tx.commit();
         } catch (Exception e) {
